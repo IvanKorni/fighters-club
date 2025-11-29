7325007668bf42f8802601cdc3cf4e43d159c2ec
konishchukii <konishchukII@idft.ru>
2025-11-29 22:21:03 +0100

    Добавлены базовые Java классы для game-service

---
+++ game-service/src/main/java/net/proselyte/gameservice/config/AppConfig.java
@@ -0,0 +1,9 @@
+package net.proselyte.gameservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // Configuration for Game Service
+}
+
