39ce0e54a7b95aa28f73044ce748d4f5a8495bb9
konishchukii <konishchukII@idft.ru>
2025-11-29 22:50:39 +0100

    Добавлены базовые Java классы для stats-service

---
+++ stats-service/src/main/java/net/proselyte/statsservice/config/AppConfig.java
@@ -0,0 +1,9 @@
+package net.proselyte.statsservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // Configuration for Stats Service
+}
+
