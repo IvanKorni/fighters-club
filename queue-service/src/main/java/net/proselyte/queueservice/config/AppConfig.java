34f7e2e793173825654a7b08bcfa9f614078e1a7
konishchukii <konishchukII@idft.ru>
2025-12-01 22:23:34 +0100

    delete queue-service

--- queue-service/src/main/java/net/proselyte/queueservice/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package net.proselyte.queueservice.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-    // Configuration for Queue Service
-}
-
