ea1581257863382bd36331a8e4e243159af9e6e9
konishchukii <konishchukII@idft.ru>
2025-12-13 21:46:38 +0100

    test(game-service): добавлены тесты для API создания матча
    - Добавлены unit-тесты для MatchService
    - Добавлены интеграционные тесты для GameRestControllerV1
    - Добавлены валидационные аннотации в CreateMatchRequest
    - Тесты проверяют успешное создание матча и обработку ошибок валидации

--- game-service/src/main/java/net/proselyte/gameservice/dto/CreateMatchRequest.java
+++ game-service/src/main/java/net/proselyte/gameservice/dto/CreateMatchRequest.java
@@ -3,12 +3,16 @@ package net.proselyte.gameservice.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class CreateMatchRequest {
+    @NotNull(message = "player1Id is required")
     private UUID player1Id;
+    
+    @NotNull(message = "player2Id is required")
     private UUID player2Id;
 }
 
